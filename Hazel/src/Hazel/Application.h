1274b4abd6e6b81d01f9f510215972612b6365af
roby909 <aradirobert123@gmail.com>
2019-01-20 12:59:59 +0100

    many changes

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Core.h"
+#include "LayerStack.h"
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+#include "Window.h"
 
 namespace Hazel {
 
@@ -11,6 +15,22 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
+
+		inline static Application& Get() { return *s_Instance; }
+		inline Window& GetWindow() { return *m_Window; }
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
+		LayerStack m_LayerStack;
+
+		static Application* s_Instance;
 	};
 
 	// To be defined in the client
